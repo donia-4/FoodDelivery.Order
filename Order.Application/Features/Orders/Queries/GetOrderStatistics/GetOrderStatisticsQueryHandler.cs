4a85cf5e06dfed217acda77d642358404d827179
Donia Shaban <doniashaban723@gmail.com>
2026-08-04 04:04:06 +0300

    test

--- Order.Application/Features/Orders/Queries/GetOrderStatistics/GetOrderStatisticsQueryHandler.cs
+++ Order.Application/Features/Orders/Queries/GetOrderStatistics/GetOrderStatisticsQueryHandler.cs
@@ -1,21 +1,33 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Logging;
 using Order.Application.Common.Interfaces.Repositories;
 using Order.Application.Features.Orders.Dtos.GetOrderStatistics;
 using Order.Domain.Results;
 
 namespace Order.Application.Features.Orders.Queries.GetOrderStatistics;
 
-public sealed class GetOrderStatisticsQueryHandler(IOrderRepository orderRepository)
+public sealed class GetOrderStatisticsQueryHandler(IOrderRepository orderRepository
+    ,ILogger<GetOrderStatisticsQueryHandler> logger)
     : IRequestHandler<GetOrderStatisticsQuery, Result<OrderStatisticsDto>>
 {
     public async Task<Result<OrderStatisticsDto>> Handle(
         GetOrderStatisticsQuery request,
         CancellationToken cancellationToken)
     {
+        logger.LogInformation(
+            "Fetching order statistics from {FromDate} to {ToDate}",
+            request.FromDate,
+            request.ToDate);
+
         var statistics = await orderRepository.GetStatisticsAsync(
             request.FromDate,
             request.ToDate,
             cancellationToken);
+
+        logger.LogInformation(
+                "Order statistics retrieved successfully for date range {FromDate} to {ToDate}",
+                request.FromDate,
+                request.ToDate);
 
         return statistics;
     }
