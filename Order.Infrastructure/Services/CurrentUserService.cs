7e40c1104164df6e7d2c9efcd4f3b6c273236098
Donia Shaban <doniashaban723@gmail.com>
2026-08-05 02:20:03 +0300

    refactor(orders): introduce current user service abstraction

---
+++ Order.Infrastructure/Services/CurrentUserService.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+
+using Order.Application.Common.Interfaces.Services;
+
+namespace Order.Infrastructure.Services;
+public sealed class CurrentUserService : ICurrentUserService
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public Guid? UserId
+    {
+        get
+        {
+            var value =
+                _httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier)
+                ?? _httpContextAccessor.HttpContext?.User.FindFirstValue("sub");
+
+            return Guid.TryParse(value, out var id)
+                ? id
+                : null;
+        }
+    }
+
+    public string? UserName =>
+        _httpContextAccessor.HttpContext?.User.Identity?.Name
+        ?? _httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.Name)
+        ?? _httpContextAccessor.HttpContext?.User.FindFirstValue("name")
+        ?? _httpContextAccessor.HttpContext?.User.FindFirstValue("preferred_username");
+}
